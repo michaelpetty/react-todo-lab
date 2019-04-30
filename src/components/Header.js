5e6f5e8cb28005fc6ace22944fbdf04d5bf24136
mpetty <michael.petty@gmail.com>
2019-04-30 11:13:52 -0700

    mv routes to config

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Header = () => (
+  <header>
+    <h1>ToDo</h1>
+    <nav>
+      <Link to={'/'}>Home</Link>
+      <Link to={'/todos'}>Todos</Link>
+    </nav>
+  </header>
+)
+
+export default Header;
