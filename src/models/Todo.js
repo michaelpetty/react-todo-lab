2ddaffa879b5ce83094ab4df245c5d94fd4c96d9
michaelpetty <michael.petty@gmail.com>
2022-11-07 17:56:34 -0800

    add expected api res format to model

--- src/models/Todo.js
+++ src/models/Todo.js
@@ -22,3 +22,40 @@ class TodoModel {
 }
 
 export default TodoModel;
+
+
+/**
+ * expected api res format (json)
+ * 
+{
+  todos: [
+    {
+      _id: "6332719dbe0ff800176d5608",
+      body: "Wash the dishes",
+      priority: 4,
+      completed: false,
+      __v: 0
+    },
+    {
+      _id: "6332719dbe0ff800176d560a",
+      body: "Gaze at the stars",
+      priority: 5,
+      completed: false,
+      __v: 0
+    },
+    {
+      _id: "633a6e57caad6e0017f00986",
+      body: "finish this to do app!",
+      completed: false,
+      __v: 0
+    },
+    {
+      _id: "633a6e5bcaad6e0017f00987",
+      body: "lol",
+      completed: false,
+      __v: 0
+    }
+  ]
+}
+ * 
+ */
